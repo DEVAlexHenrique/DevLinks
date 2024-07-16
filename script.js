f0d25883b062e450706dc384971cc4abe03e5a4d
DEVAlexHenrique <alex.henrique1212@gmail.com>
2024-07-16 16:29:03 -0300

    Added project access link in readme and avatar image adjustment

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ function toggleMode() {
     const alt = document.querySelector("#profile img")
 
     if(html.classList.contains("ligth")) {
-        img.setAttribute("src","./assets/avatar-light.png")
+        img.setAttribute("src","../assets/avatar-light.png")
         alt.setAttribute ("alt","Imagem do Alex com fundo preto!")
     } else {
-        img.setAttribute("src","./assets/avatar.png")
+        img.setAttribute("src","../assets/avatar.png")
         alt.setAttribute ("alt","Imagem do Alex com fundo branco!")
     }
     
