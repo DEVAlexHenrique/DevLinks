f63a6792bbaf01e5f271b49dfac25fa0b9780abe
DEVAlexHenrique <alex.henrique1212@gmail.com>
2024-07-16 16:41:39 -0300

    avatar image adjustment

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ function toggleMode() {
     const alt = document.querySelector("#profile img")
 
     if(html.classList.contains("ligth")) {
-        img.setAttribute("src","../assets/avatar-light.jpg")
+        img.setAttribute("src","./assets/avatar-light.jpg")
         alt.setAttribute ("alt","Imagem do Alex com fundo preto!")
     } else {
-        img.setAttribute("src","../assets/avatar.jpg")
+        img.setAttribute("src","./assets/avatar.jpg")
         alt.setAttribute ("alt","Imagem do Alex com fundo branco!")
     }
     
